340dfd4b496134c95562e893032644c57678aaba
zahar23go <zahar-c23@mail.ru>
2026-01-10 01:56:05 +0300

    Урок 2

---
+++ 02_lesson_task_2_leap_year.py
@@ -0,0 +1,9 @@
+﻿def is_year_leap(year):
+    return year % 4 == 0
+
+
+years = [2024, 2023]
+
+for y in years:
+    result = is_year_leap(y)
+    print(f"год {y}: {result}")
---
+++ 02_lesson_task_3_square_area.py
@@ -0,0 +1,6 @@
+﻿import math
+
+def square(side):
+    return math.ceil(side * side)
+
+print(f"Площадь: {square(5.2)}")
