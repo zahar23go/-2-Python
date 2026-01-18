3d100a940f209ab8e11f5258ecb47f7f982ba019
zahar23go <zahar-c23@mail.ru>
2026-01-18 21:28:19 +0300

    Урок 3: ООП в Python - создание класса, список объектов, вложенные классы

---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,12 @@
+﻿from smartphone import Smartphone
+
+catalog = []
+
+catalog.append(Smartphone("Samsung", "Galaxy S23", "+79123456789"))
+catalog.append(Smartphone("Apple", "iPhone 15", "+79234567890"))
+catalog.append(Smartphone("Xiaomi", "Redmi Note 12", "+79345678901"))
+catalog.append(Smartphone("Google", "Pixel 8", "+79456789012"))
+catalog.append(Smartphone("OnePlus", "11 Pro", "+79567890123"))
+
+for phone in catalog:
+    print(f"{phone.brand} - {phone.model}. {phone.phone_number}")
---
+++ 03_lesson/lesson_3_task_3.py
@@ -0,0 +1,9 @@
+﻿from address import Address
+from mailing import Mailing
+
+to_address = Address("123456", "Москва", "Ленина", "10", "25")
+from_address = Address("654321", "Санкт-Петербург", "Невский", "15", "3")
+
+mailing = Mailing(to_address, from_address, 350.50, "TR123456789RU")
+
+print(f"Отправление {mailing.track} из {mailing.from_address.index}, {mailing.from_address.city}, {mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartment} в {mailing.to_address.index}, {mailing.to_address.city}, {mailing.to_address.street}, {mailing.to_address.house} - {mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.")
