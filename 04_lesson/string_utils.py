5d9eb73b455fb8350f0c17a6628c0c83bb06a792
zahar23go <zahar-c23@mail.ru>
2026-01-31 16:09:44 +0300

    Add lesson4 homework: StringUtils class with tests

---
+++ 04_lesson/string_utils.py
@@ -0,0 +1,12 @@
+﻿class StringUtils:
+    """
+    Класс с полезными утилитами для обработки и анализа строк
+    """
+
+    def capitalize(self, string: str) -> str:
+        """
+        Принимает на вход текст, делает первую букву заглавной
+        и возвращает этот же текст
+        Пример: `capitalize("skypro") -> "Skypro"`
+        """
+        return string.capitalize()
