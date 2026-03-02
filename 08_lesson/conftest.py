bcace3765dbfd87bfac08205d3e9e1267c7b8a84
zahar23go <zahar-c23@mail.ru>
2026-03-02 21:58:50 +0300

    Add ProjectAPI class, fix fixtures, add gitignore, add README_08_lesson

--- 08_lesson/conftest.py
+++ 08_lesson/conftest.py
@@ -1,17 +1,3 @@
-﻿import pytest
-import os
-
-BASE_URL = "https://yougile.com/api-v2"
-
-
-@pytest.fixture
+﻿@pytest.fixture(scope="session")
 def base_url():
-    return BASE_URL
-
-
-@pytest.fixture
-def auth_token():
-    token = os.getenv("YOUGILE_TOKEN")
-    if not token:
-        pytest.skip("YOUGILE_TOKEN is not set")
-    return token
+    return 
